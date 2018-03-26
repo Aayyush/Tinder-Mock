420ccfb435476e82329f1d307c18132a5c3ebe8e
Aayush  Gupta <aayushgupta@Aayushs-MacBook-Pro.local>
2018-03-26 01:12:21 -0400

    Added the GIF to README.md

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ The following **stretch** user stories are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://i.imgur.com/7dUErqT.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
--- Tinder-mock/CardsViewController.swift
+++ Tinder-mock/CardsViewController.swift
@@ -11,38 +11,53 @@ import UIKit
 class CardsViewController: UIViewController {
     
     var cardInitialCenter: CGPoint!
-    var tapPoint: CGPoint!
+    var tapPointInitial: CGPoint!
+    var tapPointFinal: CGPoint!
+    
     @IBOutlet weak var userImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         userImage.isUserInteractionEnabled = true
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(sender:)))
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(sender:)))
-        userImage.addGestureRecognizer(tapGestureRecognizer)
         userImage.addGestureRecognizer(panGestureRecognizer)
         // Do any additional setup after loading the view.
     }
-    
-    @objc func didTap(sender: UITapGestureRecognizer){
-        if sender.state == .recognized{
-            print("Tap Recognized")
-        }else if sender.state == .ended{
-            print(sender.location(in: view))
-        }
-    }
+
     
     @objc func didPan(sender: UIPanGestureRecognizer){
         let translation = sender.translation(in: view)
         if sender.state == UIGestureRecognizerState.began{
             cardInitialCenter = userImage.center
+            tapPointInitial = sender.location(in: view)
         }else if sender.state == UIGestureRecognizerState.changed{
-            // getImageRotation(start: CGPoint, end: <#T##CGPoint#>, center: cardInitialCenter)
-            userImage.center = CGPoint(x: cardInitialCenter.x + translation.x , y: cardInitialCenter.y + translation.y)
-            if translation.x > 0{
-             userImage.transform = CGAffineTransform(rotationAngle: CGFloat(45 * Double.pi/180))
+            tapPointFinal = sender.location(in: view)
+            var rotationAngle = getImageRotation(start: tapPointInitial, end: tapPointFinal, center: cardInitialCenter)
+            if rotationAngle > Double.pi/6{
+                // Avoid excessive rotation.
+                rotationAngle = Double.pi/6
+            }
+            if translation.y > 0{
+               userImage.center = CGPoint(x: cardInitialCenter.x + translation.x , y: cardInitialCenter.y + translation.y)
             }else{
-                userImage.transform = CGAffineTransform(rotationAngle: CGFloat(-45 * Double.pi/180))
+               userImage.center = CGPoint(x: cardInitialCenter.x + translation.x , y: cardInitialCenter.y - translation.y)
+            }
+            if translation.x > 0{
+             userImage.transform = CGAffineTransform(rotationAngle: CGFloat(rotationAngle))
+                if translation.x >= 50{
+                    UIView.animate(withDuration: 0.3, animations: {
+                        self.userImage.alpha = 0
+                    })
+                }
+            }else{
+                if translation.x <= -50{
+                    if translation.x >= 50{
+                        UIView.animate(withDuration: 0.3, animations: {
+                            self.userImage.alpha = 0
+                        })
+                    }
+                }
+                userImage.transform = CGAffineTransform(rotationAngle: -CGFloat(rotationAngle))
             }
         }else if sender.state == UIGestureRecognizerState.ended{
             userImage.center = cardInitialCenter
@@ -50,17 +65,33 @@ class CardsViewController: UIViewController {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "profileSegue"{
+            let destination = segue.destination as! ProfileViewController
+           destination.profileImageURL = "ryan"
+        }
+    }
+    
+    @IBAction func didTapProfile(_ sender: Any) {
+        self.performSegue(withIdentifier: "profileSegue", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func getImageRotation(start: CGPoint, end: CGPoint, center: CGPoint){
-        let initialDistance = Double((pow(start.x - center.x, 2) - pow(start.y - center.y, 2)))
-        let finalDistance = Double((pow(end.x - center.x, 2) - pow(end.y - center.y, 2)))
-        let intermediateDistance = Double((pow(end.x - start.x, 2) - pow(end.y - start.y, 2)))
-        let rotationAngle = acos((initialDistance + finalDistance - intermediateDistance)/2*sqrt(initialDistance)*sqrt(finalDistance))
-        print(rotationAngle)
+    func getImageRotation(start: CGPoint, end: CGPoint, center: CGPoint) -> Double{
+        // print("Image Center: \(center) Start Point: \(start) End Point \(end)")
+        let initialDistance = Double((pow(start.x - center.x, 2) + pow(start.y - center.y, 2)))
+        // print(initialDistance)
+        let finalDistance = Double((pow(end.x - center.x, 2) + pow(end.y - center.y, 2)))
+        // print(finalDistance)
+        let intermediateDistance = Double((pow(end.x - start.x, 2) + pow(end.y - start.y, 2)))
+        // print(intermediateDistance)
+        let rotationAngle = acos((initialDistance + finalDistance - intermediateDistance)/(2*sqrt(initialDistance)*sqrt(finalDistance)))
+        // print("Rotation Angle: \(rotationAngle)")
+        return rotationAngle
     }
     
     /*
